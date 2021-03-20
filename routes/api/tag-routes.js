4bd70cdf7a35dfc47f6d61b2c3f1c1301b3d9111
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-03-20 10:50:14 -0500

    added post route and tested functionality

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -41,8 +41,19 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+router.post('/', async (req, res) => {
+  try {
+    const tagData = await Tag.create(req.body, {
+      include: [
+        {
+          model: Product
+        }
+      ]
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
